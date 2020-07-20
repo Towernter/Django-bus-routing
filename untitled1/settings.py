53c7e0c478e06cfecc9abaf1478f36094cf8aaf2
Tawanda Nyahuye <towernter@gmail.com>
2020-07-20 12:18:40 +0200

    Update settings.py
    added Heroku settings

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
